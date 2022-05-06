24b1a22060949e3167f482d2e334f88c6436ab32
kamil jarrouge <zackrowland94@gmail.com>
2022-05-06 22:59:42 +0300

    removed read column from reminders table and removed the condition on the FSEreminders function regarding the read column

--- database/migrations/2022_04_11_165611_create_reminders_table.php
+++ database/migrations/2022_04_11_165611_create_reminders_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id');
             $table->foreignId('file_system_entry_id');
-            $table->boolean('read');
             $table->timestamps();
         });
     }
