064a67649cbcf8013fc4753e574388ca3dde32f0
bashor <bashor.developer@gmail.com>
2022-05-12 00:15:03 +0300

    blah

--- routes/api.php
+++ routes/api.php
@@ -66,7 +66,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/{fileSystemEntry}', [FileSystemEntryController::class, 'index']);
         Route::get('/{fileSystemEntry}/versions', [FileSystemEntryController::class, 'versions']);
         Route::post('/{fileSystemEntry}/versions/add', [FileSystemEntryController::class, 'add_version']);
-        Route::get('/media/{media}/download', [FileSystemEntryController::class, 'download']);
+        Route::get('/{fileSystemEntry}/media/{media}/download', [FileSystemEntryController::class, 'download']);
         Route::get('/media/{media}/download/last', [FileSystemEntryController::class, 'download_latest']);
         Route::delete('/{fileSystemEntry}/versions/{version}/delete', [FileSystemEntryController::class, 'delete_version']);
         Route::get('/{fileSystemEntry}/show', [FileSystemEntryController::class, 'show']);
